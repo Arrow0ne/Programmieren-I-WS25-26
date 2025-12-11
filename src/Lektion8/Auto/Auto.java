abeeaa38a03b9a03903a517218cdb9958d8682ca
Arrow0ne <arturwacker7@gmail.com>
2025-12-11 11:55:28 +0100

    Auto

--- src/Lektion8/Auto/Auto.java
+++ src/Lektion8/Auto/Auto.java
@@ -7,16 +7,42 @@ public class Auto {
 
     public Auto(String Marke, int Hubraum, String Farbe) {
         this.Marke = Marke;
-        this.Hubraum = Hubraum;
+        this.Farbe = Farbe;
+        if (Hubraum <= 0) {
+            this.Hubraum = 1600;
+        }
+    }
+
+    public String getMarke() {
+        return Marke;
+    }
+
+    public void setMarke(String Marke) {
+        this.Marke = Marke;
+    }
+
+    public int getHubraum() {
+        return Hubraum;
+    }
+
+    public void setHubraum(int Hubraum) {
+        if (Hubraum > 0) {
+            this.Hubraum = Hubraum;
+        }
+    }
+
+    public String getFarbe() {
+        return Farbe;
+    }
+
+    public void setFarbe(String Farbe) {
         this.Farbe = Farbe;
     }
 
-    public void printDaten(){
+    public void printDaten() {
         System.out.println("Marke: " + Marke);
         System.out.println("Hubraum: " + Hubraum + " ccm");
         System.out.println("Farbe: " + Farbe);
         System.out.println();
-
     }
-
 }
