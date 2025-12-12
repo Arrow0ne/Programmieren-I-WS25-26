2fe00cc1eabeaea0124f23483fd3bbf36010f8f0
Arrow0ne <arturwacker7@gmail.com>
2025-12-12 22:25:18 +0100

    Raumbelegung

--- src/Lektion9/Raum/Raum.java
+++ src/Lektion9/Raum/Raum.java
@@ -1,16 +1,30 @@
 package Lektion9.Raum;
 
 public class Raum {
-    String Raum;
-    int maxAnzahl;
-    int[] Belegung;
+    private String Raum;
+    private int maxAnzahl;
+    private boolean belegt;
 
-    public int getMax() {
-        return maxAnzahl;
-    }
-
-    public Raum(String Raum, int maxAnzahl) {
+    public Raum(String Raum, int maxAnzahl, boolean belegt) {
         this.Raum = Raum;
         this.maxAnzahl = maxAnzahl;
+        this.belegt = belegt;
+    }
+
+    public void belege(){
+        this.belegt = true;
+    }
+    public void gebeFrei(){
+        this.belegt = false;
+    }
+
+    public void druckeStatus() {
+        if (!belegt) {
+            System.out.println("Der Raum " + Raum +
+                    " ist nicht belegt und kann maximal " +
+                    maxAnzahl + " Studenten haben.");
+        } else {
+            System.out.println("Der Raum " + Raum + " ist belegt.");
+        }
     }
 }
--- src/Lektion9/Raum/Raumbelegung.java
+++ src/Lektion9/Raum/Raumbelegung.java
@@ -1,8 +1,16 @@
 package Lektion9.Raum;
 
-Raum[] Menge = createStudenten();
+public class Raumbelegung {
+    public static void main(String[] args) {
+        Raum raum1 = new Raum("l.2.1", 200, false);
+        Raum raum2 = new Raum("l.2.15", 250, false);
 
-public static Raum[] createStudenten() {
+        raum1.druckeStatus();
+        raum2.druckeStatus();
 
-}
+        raum1.belege();
+
+        raum1.druckeStatus();
+        raum2.druckeStatus();
+    }
 }
