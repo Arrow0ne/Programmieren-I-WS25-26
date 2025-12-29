d655f369499b5a77045ea96c9f217305d80a98a1
Arrow0ne <arturwacker7@gmail.com>
2025-12-29 16:12:47 +0100

    Lektion 11

--- src/Lektion11/UeberladenDerBetragsmethode.java
+++ src/Lektion11/UeberladenDerBetragsmethode.java
@@ -1,4 +1,46 @@
 package Lektion11;
 
 public class UeberladenDerBetragsmethode {
+    public static int betrag(int x) {
+        if (x < 0) {
+            return -x;
+        }
+        return x;
+    }
+
+    public static float betrag(float x) {
+        if (x < 0) {
+            return -x;
+        }
+        return x;
+    }
+
+    public static double betrag(double x) {
+        if (x < 0) {
+            return -x;
+        }
+        return x;
+    }
+
+    public static short betrag(short x) {
+        if (x < 0) {
+            return (short) -x;
+        }
+        return x;
+    }
+
+    public static long betrag(long x){
+        if (x < 0){
+            return -x;
+        }
+        return x;
+    }
+
+    public static void main(String[] args){
+        System.out.println(betrag(-5));
+        System.out.println(betrag(-10.5f));
+        System.out.println(betrag(-7.843));
+        System.out.println(betrag((short) -8));
+        System.out.println(betrag(1000000L));
+    }
 }
--- src/Lektion11/Zeilenmaxima.java
+++ src/Lektion11/Zeilenmaxima.java
@@ -1,4 +1,38 @@
 package Lektion11;
 
 public class Zeilenmaxima {
+
+    public static double[] zeilenMaxima(double[][] matrix) {
+
+        double[] maxWerte = new double[matrix.length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            double max = matrix[i][0];
+
+            for (int j = 1; j < matrix[i].length; j++) {
+                if (matrix[i][j] > max) {
+                    max = matrix[i][j];
+                }
+            }
+
+            maxWerte[i] = max;
+        }
+
+        return maxWerte;
+    }
+
+    public static void main(String[] args) {
+
+        double[][] matrix = {
+                {-2.3, 0.555, 2324, -0.4502},
+                {3123, 453453, -23546, 23245, 3435}
+        };
+
+        double[] ergebnis = zeilenMaxima(matrix);
+
+        for (int i = 0; i < ergebnis.length; i++) {
+            System.out.println(ergebnis[i]);
+        }
+    }
 }
+
